06791fe9f91191971f65f3d2f1d32d0b2cd3ca31
Feng <15951605212@163.com>
2020-12-07 14:45:38 +0800

    first commit

---
+++ fyx/src/main/java/com/adam/fyx/AdamTest.java
@@ -0,0 +1,15 @@
+package com.adam.fyx;
+
+/**
+ * Author: Adam Inc.
+ * <br/>
+ * Created at: 2020/12/7 14:25
+ * <br/>
+ *
+ * @since
+ */
+public class AdamTest {
+    public int calculation(int number){
+        return  number*2;
+    }
+}
